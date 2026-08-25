cefe37cb2c6543344f34d64c3a79e6717a202fa4
dadou000 <david.f.francois@gmail.com>
2026-08-26 01:42:35 +0200

    Expose point wind sampling for vehicle physics

--- native/weather/src/weather_wind_sampler.h
+++ native/weather/src/weather_wind_sampler.h
@@ -3,6 +3,7 @@
 #include "weather_native.h"
 #include <godot_cpp/classes/object.hpp>
 #include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/variant/vector3.hpp>
 
 namespace godot {
 
@@ -14,6 +15,7 @@ protected:
 
 public:
 	PackedFloat32Array get_global_wind_rgba(Object *weather, int layer) const;
+	Vector3 sample_wind(Object *weather, const Vector3 &direction, float altitude_m) const;
 };
 
 } // namespace godot
