f18becd8463c47ca16853e5d533d726375a1485b
dadou000 <david.f.francois@gmail.com>
2026-08-30 14:58:33 +0200

    Expose foundation policy helpers

--- experiments/locomotion_19body/training/asterra_rl/__init__.py
+++ experiments/locomotion_19body/training/asterra_rl/__init__.py
@@ -1,5 +1,6 @@
 """Repo-local Isaac Lab helpers for the Asterra 19-body humanoid experiment."""
 
+from .actions import CanonicalActionProcessor
 from .articulation import (
     canonical_to_physx_joint_ids,
     expected_body_names,
@@ -10,14 +11,22 @@ from .articulation import (
     standing_root_height_m,
     validate_live_articulation,
 )
+from .observations import KEY_BODY_NAMES, OBSERVATION_SIZE, build_foundation_observation
+from .rewards import compute_stand_reward, stand_termination
 
 __all__ = [
+    "CanonicalActionProcessor",
+    "KEY_BODY_NAMES",
+    "OBSERVATION_SIZE",
+    "build_foundation_observation",
     "canonical_to_physx_joint_ids",
+    "compute_stand_reward",
     "expected_body_names",
     "expected_dof_names",
     "load_articulation_manifest",
     "make_articulation_cfg",
     "resolve_training_usd",
+    "stand_termination",
     "standing_root_height_m",
     "validate_live_articulation",
 ]
