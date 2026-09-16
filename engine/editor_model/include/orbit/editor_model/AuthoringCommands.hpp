78b70290a7ef082b11ed7826549e06215b12b2a2
dadou000 <david.f.francois@gmail.com>
2026-09-16 11:39:31 +0200

    Add shared material assignment command ID

--- engine/editor_model/include/orbit/editor_model/AuthoringCommands.hpp
+++ engine/editor_model/include/orbit/editor_model/AuthoringCommands.hpp
@@ -27,6 +27,11 @@ inline constexpr commands::CommandId kMoveToRoot{
     .low = 0x4d4f5645524f4f54ULL
 };
 
+inline constexpr commands::CommandId kAssignMaterial{
+    .high = 0x4f52424954434d44ULL,
+    .low = 0x41535349474e4d54ULL
+};
+
 void Register(
     commands::CommandRegistry& registry,
     commands::CommandService& commandService,
