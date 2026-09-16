d1bc73dbf5f944a866ca39f2953f75ff9f271cef
dadou000 <david.f.francois@gmail.com>
2026-09-16 11:41:22 +0200

    Test malformed content isolation

--- engine/content/tests/ContentServiceTests.cpp
+++ engine/content/tests/ContentServiceTests.cpp
@@ -35,6 +35,8 @@ int main()
     Write(root / "Content" / "Materials" / "steel_base.ktx2", "texture");
     Write(root / "Content" / "Materials" / "steel_normal.ktx2", "texture");
     Write(root / "Content" / "Materials" / "steel_rough.ktx2", "texture");
+    Write(root / "Content" / "Materials" / "broken.orbitmaterial",
+        "[not_material]\nname = \"Broken\"\n");
 
     orbit::content::ContentService content(root);
     content.Scan();
@@ -44,6 +46,9 @@ int main()
     assert(materials[0].material.has_value());
     assert(materials[0].material->metallicFactor == 1.0);
     assert(materials[0].tags.size() == 2);
+    assert(content.Diagnostics().size() == 1);
+    assert(content.Diagnostics()[0].sourcePath ==
+        std::filesystem::path("Content/Materials/broken.orbitmaterial"));
 
     const auto stableId = materials[0].id;
     content.Scan();
