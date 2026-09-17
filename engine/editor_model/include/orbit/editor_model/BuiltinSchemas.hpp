7ce4ab2b70dd4c8d58e96b80b9566a09198750f4
dadou000 <david.f.francois@gmail.com>
2026-09-17 15:18:01 +0200

    Expose surface decal schema to editor model

--- engine/editor_model/include/orbit/editor_model/BuiltinSchemas.hpp
+++ engine/editor_model/include/orbit/editor_model/BuiltinSchemas.hpp
@@ -13,6 +13,8 @@ inline constexpr schema::TypeId kCelestialSystemType =
     world_model::kCelestialSystemType;
 inline constexpr schema::TypeId kCelestialBodyType =
     world_model::kCelestialBodyType;
+inline constexpr schema::TypeId kSurfaceDecalType =
+    world_model::kSurfaceDecalType;
 
 inline constexpr schema::PropertyId kSystemEpochMicroseconds =
     world_model::kSystemEpochMicroseconds;
@@ -34,6 +36,20 @@ inline constexpr schema::PropertyId kBodyRotationPhaseDegrees =
     world_model::kBodyRotationPhaseDegrees;
 inline constexpr schema::PropertyId kBodyMaterialAsset =
     world_model::kBodyMaterialAsset;
+inline constexpr schema::PropertyId kDecalAsset =
+    world_model::kDecalAsset;
+inline constexpr schema::PropertyId kDecalLatitudeRadians =
+    world_model::kDecalLatitudeRadians;
+inline constexpr schema::PropertyId kDecalLongitudeRadians =
+    world_model::kDecalLongitudeRadians;
+inline constexpr schema::PropertyId kDecalWidthMeters =
+    world_model::kDecalWidthMeters;
+inline constexpr schema::PropertyId kDecalHeightMeters =
+    world_model::kDecalHeightMeters;
+inline constexpr schema::PropertyId kDecalRotationDegrees =
+    world_model::kDecalRotationDegrees;
+inline constexpr schema::PropertyId kDecalOpacity =
+    world_model::kDecalOpacity;
 
 void RegisterSchemas(
     schema::SchemaRegistry& schemas);
