665bb44d28dc0c29bbda037d803d60225a96ed5d
dadou000 <david.f.francois@gmail.com>
2026-09-17 13:27:21 +0200

    Keep celestial authoring schema runtime-backed

--- engine/editor_model/include/orbit/editor_model/BuiltinSchemas.hpp
+++ engine/editor_model/include/orbit/editor_model/BuiltinSchemas.hpp
@@ -19,24 +19,14 @@ inline constexpr schema::TypeId kCelestialBodyType{
     .low = 0x5954595045000001ULL
 };
 
-inline constexpr schema::PropertyId kWorldEpochMicroseconds{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x505745504f434801ULL
-};
-
-inline constexpr schema::PropertyId kWorldTimeScale{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x505754494d455301ULL
-};
-
 inline constexpr schema::PropertyId kSystemEpochMicroseconds{
     .high = 0x4f5242495450524fULL,
     .low = 0x505345504f434801ULL
 };
 
-inline constexpr schema::PropertyId kBodyShapeMode{
+inline constexpr schema::PropertyId kBodyEllipsoidEnabled{
     .high = 0x4f5242495450524fULL,
-    .low = 0x5053484150450001ULL
+    .low = 0x50454c4c49505301ULL
 };
 
 inline constexpr schema::PropertyId kBodyRadius{
@@ -54,6 +44,11 @@ inline constexpr schema::PropertyId kBodyMass{
     .low = 0x504d415353000001ULL
 };
 
+inline constexpr schema::PropertyId kBodyParentPositionMeters{
+    .high = 0x4f5242495450524fULL,
+    .low = 0x50504152454e5401ULL
+};
+
 inline constexpr schema::PropertyId kBodyRotationPeriodSeconds{
     .high = 0x4f5242495450524fULL,
     .low = 0x50524f5450455201ULL
@@ -64,34 +59,9 @@ inline constexpr schema::PropertyId kBodyAxialTiltDegrees{
     .low = 0x5054494c54444501ULL
 };
 
-inline constexpr schema::PropertyId kBodySurfaceEnabled{
+inline constexpr schema::PropertyId kBodyRotationPhaseDegrees{
     .high = 0x4f5242495450524fULL,
-    .low = 0x5053555246454e01ULL
-};
-
-inline constexpr schema::PropertyId kBodyAtmosphereEnabled{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x5041544d4f454e01ULL
-};
-
-inline constexpr schema::PropertyId kBodyHydrosphereEnabled{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x5048594452454e01ULL
-};
-
-inline constexpr schema::PropertyId kBodyTerrainSeed{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x5054455253454501ULL
-};
-
-inline constexpr schema::PropertyId kBodyOceanLevelMeters{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x504f4345414e4c01ULL
-};
-
-inline constexpr schema::PropertyId kBodyMaximumElevationMeters{
-    .high = 0x4f5242495450524fULL,
-    .low = 0x504d4158454c4501ULL
+    .low = 0x5050484153450001ULL
 };
 
 inline constexpr schema::PropertyId kBodyMaterialAsset{
