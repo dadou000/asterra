68de5756e30c2444abc8fcb8121a8e303b1f49e5
dadou000 <david.f.francois@gmail.com>
2026-09-18 12:26:06 +0200

    Add DXC coverage for M09 drainage shaders

---
+++ engine/terrain_gpu/tests/M09DrainageShaderTests.cpp
@@ -0,0 +1,52 @@
+#include <orbit/shader/dxc/DxcShaderCompiler.hpp>
+
+#include "../src/M09DrainageCompute.hpp"
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    const orbit::shader::dxc::DxcShaderCompiler compiler;
+
+    constexpr std::array<const char*, 5> shaders{
+        orbit::terrain_gpu::detail::kM09ExtractSurfaceShader,
+        orbit::terrain_gpu::detail::kM09DownstreamShader,
+        orbit::terrain_gpu::detail::kM09InitializeFlowShader,
+        orbit::terrain_gpu::detail::kM09BuildDonorsShader,
+        orbit::terrain_gpu::detail::kM09RakeCompressShader
+    };
+
+    for (std::size_t i = 0; i < shaders.size(); ++i)
+    {
+        const auto binary = compiler.Compile({
+            .source = shaders[i],
+            .entryPoint = "main",
+            .stage = orbit::shader::Stage::Compute,
+            .debug = false
+        });
+
+        if (binary.bytecode.empty())
+        {
+            std::cerr
+                << "M09 compute shader "
+                << i
+                << " produced empty DXIL/SPIR-V bytecode.\n";
+            return EXIT_FAILURE;
+        }
+
+        if (binary.stage != orbit::shader::Stage::Compute)
+        {
+            std::cerr
+                << "M09 compute shader "
+                << i
+                << " returned incorrect stage metadata.\n";
+            return EXIT_FAILURE;
+        }
+    }
+
+    std::cout
+        << "Orbit M09 drainage compute shaders compiled successfully.\n";
+    return EXIT_SUCCESS;
+}
