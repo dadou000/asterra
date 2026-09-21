c11bbff32194de6bc3eb329dc4194b3ef1d90091
dadou000 <david.f.francois@gmail.com>
2026-09-21 17:48:09 +0200

    Extend macro globe lighting with M24 ocean optics

--- engine/celestial_globe/include/orbit/celestial_globe/MacroGlobe.hpp
+++ engine/celestial_globe/include/orbit/celestial_globe/MacroGlobe.hpp
@@ -87,6 +87,10 @@ struct MacroGlobeLighting
     math::Float3 directionBody{
         0.55F, 0.72F, -0.48F};
     f32 irradianceScale{1.0F};
+    f32 oceanRefractiveIndex{1.333F};
+    f32 oceanRoughness{0.12F};
+    f32 oceanGlintStrength{1.0F};
+    bool oceanEnabled{false};
 };
 
 class MacroGlobeRenderer
