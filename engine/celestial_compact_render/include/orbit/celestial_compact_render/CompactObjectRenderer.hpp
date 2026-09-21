f4635d00f55a81a70a13e3b5231a377c9659f12b
dadou000 <david.f.francois@gmail.com>
2026-09-21 20:06:07 +0200

    M30 add analytic compact-object renderer

---
+++ engine/celestial_compact_render/include/orbit/celestial_compact_render/CompactObjectRenderer.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <orbit/celestial_compact_objects/CompactObject.hpp>
+#include <orbit/render_view/RenderView.hpp>
+#include <orbit/rhi/Command.hpp>
+#include <orbit/rhi/Device.hpp>
+#include <orbit/shader/ShaderCompiler.hpp>
+
+#include <memory>
+#include <optional>
+
+namespace orbit::celestial_compact_render
+{
+struct CompactObjectDraw
+{
+    celestial_compact_objects::
+        CompactObjectPresentation compact{};
+    std::optional<
+        celestial_compact_objects::
+            AccretionFlowParameters>
+        accretion;
+
+    render_view::CameraState camera{};
+
+    f64 projectedShadowRadiusPixels{0.0};
+    f32 opacity{1.0F};
+};
+
+class CompactObjectRenderer
+{
+public:
+    CompactObjectRenderer(
+        rhi::Device& device,
+        const shader::Compiler& compiler);
+
+    void Draw(
+        rhi::CommandList& commands,
+        rhi::Texture& target,
+        u32 width,
+        u32 height,
+        const CompactObjectDraw& draw);
+
+private:
+    std::unique_ptr<rhi::GraphicsPipeline>
+        pipeline_;
+};
+} // namespace orbit::celestial_compact_render
