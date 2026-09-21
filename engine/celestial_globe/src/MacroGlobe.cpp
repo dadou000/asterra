c1ae994fac9ebc774d143069f737fcd7012671a4
dadou000 <david.f.francois@gmail.com>
2026-09-21 14:42:06 +0200

    Declare macro-globe FP16 target format

--- engine/celestial_globe/src/MacroGlobe.cpp
+++ engine/celestial_globe/src/MacroGlobe.cpp
@@ -827,7 +827,11 @@ MacroGlobeRenderer::MacroGlobeRenderer(
         .cullMode = rhi::CullMode::Back,
         .blendMode = rhi::BlendMode::Alpha,
         .depthTest = false,
-        .depthWrite = false
+        .depthWrite = false,
+        .colorAttachmentFormats = {
+            rhi::TextureFormat::RGBA16_Float
+        },
+        .colorAttachmentCount = 1U
     });
 }
 
