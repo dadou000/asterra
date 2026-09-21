4a2ecf9d6cd0f75eb7d23b4950f165687cbed1ae
dadou000 <david.f.francois@gmail.com>
2026-09-21 11:36:28 +0200

    Expose composed gravity service and source mapping

--- engine/world_model/include/orbit/world_model/UniverseComposition.hpp
+++ engine/world_model/include/orbit/world_model/UniverseComposition.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <orbit/celestial_gravity/GravityService.hpp>
 #include <orbit/frames/FrameGraph.hpp>
 #include <orbit/scene/ObjectStore.hpp>
 #include <orbit/universe/BodyRegistry.hpp>
@@ -15,6 +16,7 @@ struct UniverseCompositionStats
     u32 systems{0};
     u32 referenceNodes{0};
     u32 bodies{0};
+    u32 gravitySources{0};
     u64 sourceRevision{0};
 };
 
@@ -54,6 +56,11 @@ public:
     [[nodiscard]] const universe::BodyRegistry&
     Bodies() const noexcept;
 
+    [[nodiscard]] celestial_gravity::GravityService&
+    Gravity() noexcept;
+    [[nodiscard]] const celestial_gravity::GravityService&
+    Gravity() const noexcept;
+
     [[nodiscard]] std::optional<universe::SystemId>
     SystemForObject(scene::ObjectId object) const noexcept;
 
@@ -63,6 +70,11 @@ public:
     [[nodiscard]] std::optional<frames::FrameId>
     FrameForObject(scene::ObjectId object) const noexcept;
 
+    [[nodiscard]] std::optional<
+        celestial_gravity::GravitySourceId>
+    GravitySourceForObject(
+        scene::ObjectId object) const noexcept;
+
     [[nodiscard]] std::optional<scene::ObjectId>
     ObjectForBody(universe::BodyId body) const noexcept;
 
@@ -71,12 +83,17 @@ public:
 private:
     std::unique_ptr<frames::FrameGraph> frames_;
     std::unique_ptr<universe::BodyRegistry> bodies_;
+    std::unique_ptr<celestial_gravity::GravityService> gravity_;
     std::unordered_map<scene::ObjectId, universe::SystemId>
         systemByObject_;
     std::unordered_map<scene::ObjectId, universe::BodyId>
         bodyByObject_;
     std::unordered_map<scene::ObjectId, frames::FrameId>
         frameByObject_;
+    std::unordered_map<
+        scene::ObjectId,
+        celestial_gravity::GravitySourceId>
+        gravitySourceByObject_;
     std::unordered_map<universe::BodyId, scene::ObjectId>
         objectByBody_;
     u64 sourceRevision_{~u64{0}};
