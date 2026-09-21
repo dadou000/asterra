96849073c4170c8dbbe08dda670b4050444c1f8f
dadou000 <david.f.francois@gmail.com>
2026-09-21 17:18:52 +0200

    Add M05 authored local light binding LocalLightBinding.hpp

---
+++ engine/world_model/include/orbit/world_model/LocalLightBinding.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <orbit/math/Vector.hpp>
+#include <orbit/scene/ObjectStore.hpp>
+
+#include <vector>
+
+namespace orbit::world_model
+{
+enum class AuthoredLightKind : u8
+{
+    Point,
+    Spot
+};
+
+struct AuthoredLocalLight
+{
+    scene::ObjectId object{};
+    AuthoredLightKind kind{AuthoredLightKind::Point};
+    math::Double3 positionMeters{};
+    math::Double3 direction{0.0, -1.0, 0.0};
+    math::Double3 colorLinear{1.0, 1.0, 1.0};
+    f64 luminousFluxLumens{0.0};
+    f64 rangeMeters{0.0};
+    f64 innerConeDegrees{0.0};
+    f64 outerConeDegrees{0.0};
+};
+
+[[nodiscard]] std::vector<AuthoredLocalLight>
+ResolveAuthoredLocalLights(
+    const scene::ObjectStore& objects);
+} // namespace orbit::world_model
