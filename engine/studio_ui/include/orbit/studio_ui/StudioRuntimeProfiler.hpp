c1040885cfc3d4c2e4a9df58e2b6051d86f6a4cc
dadou000 <david.f.francois@gmail.com>
2026-09-24 13:30:15 +0200

    M42: add volume runtime profiler snapshot

---
+++ engine/studio_ui/include/orbit/studio_ui/StudioRuntimeProfiler.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <orbit/scene/ObjectId.hpp>
+#include <orbit/volume_fields/VolumeFieldStorage.hpp>
+#include <orbit/volume_render/UniversalVolumeRenderer.hpp>
+#include <orbit/volume_solver/SurfaceVolumeSolver.hpp>
+
+namespace orbit::studio_ui
+{
+struct StudioVolumeRuntimeProfilerSnapshot
+{
+    scene::ObjectId volume{};
+    volume_fields::VolumeFieldDiagnostics fields{};
+    volume_solver::SurfaceVolumeSolverDiagnostics solver{};
+    volume_render::VolumeRenderDiagnostics renderer{};
+    u32 invalidatedTiles{0U};
+
+    bool hasSelection{false};
+    bool hasFields{false};
+    bool hasSolver{false};
+    bool hasRenderer{false};
+};
+
+[[nodiscard]] inline StudioVolumeRuntimeProfilerSnapshot&
+MutableStudioVolumeRuntimeProfiler() noexcept
+{
+    static StudioVolumeRuntimeProfilerSnapshot snapshot{};
+    return snapshot;
+}
+
+[[nodiscard]] inline const StudioVolumeRuntimeProfilerSnapshot&
+StudioVolumeRuntimeProfiler() noexcept
+{
+    return MutableStudioVolumeRuntimeProfiler();
+}
+
+inline void PublishStudioVolumeRuntimeProfiler(
+    StudioVolumeRuntimeProfilerSnapshot snapshot) noexcept
+{
+    MutableStudioVolumeRuntimeProfiler() =
+        std::move(snapshot);
+}
+
+inline void ResetStudioVolumeRuntimeProfiler() noexcept
+{
+    MutableStudioVolumeRuntimeProfiler() = {};
+}
+} // namespace orbit::studio_ui
